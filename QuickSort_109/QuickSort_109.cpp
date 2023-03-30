efe0cfab068770926a8f3fc62fb35040dae276df
PutriHabibahDevina <putrihdm13@gmail.com>
2023-03-30 15:53:16 +0700

    menampilkan angka dari data cmp dan mov

--- QuickSort_109/QuickSort_109.cpp
+++ QuickSort_109/QuickSort_109.cpp
@@ -94,3 +94,18 @@ void q_short(int low, int high)
 	q_short(j + 1, high);									//langkah algoritma no.13
 
 }
+
+void display() {
+	cout << "\n-----------------" << endl;
+	cout << "sorted Array" << endl;
+	cout << "-----------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << "\n\nNumber of comparassion: " << cmp_count << endl;
+	cout << "Number of data movements: " << mov_count << endl;
+}
+
